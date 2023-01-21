b31ef792a3f9d2b387842f682ea82331c58e66b3
boves <daiv.boveri@gmail.com>
2023-01-20 20:38:31 -0600

    Task: adding border to hero image on Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
+import { hero } from '../components/layout.module.css'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -14,7 +15,7 @@ const IndexPage = () => {
       <StaticImage
         alt="Noel Hill, holding a concertina, looking off to the side in front of a chalkboard with music notes."
         src = "../images/Noel-Hill010_md.jpg"
-        
+        className={hero}
       />
     </Layout>
   )
